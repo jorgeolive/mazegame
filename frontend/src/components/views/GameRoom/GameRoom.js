cb0b3aa3f5244007c9074fbcb39a1ce761fecd4c
jorgeolive <j.olive.rodriguez@gmail.com>
2021-01-24 11:31:11 +0100

    some educational comments on useEffect and setStat

--- frontend/src/components/views/GameRoom/GameRoom.js
+++ frontend/src/components/views/GameRoom/GameRoom.js
@@ -7,11 +7,17 @@ const ENDPOINT = "http://localhost:3000";
 
 const GameRoom = (props) => {
 
+    console.log("Runing GameRoom component");
+
+
     //https://dev.to/bravemaster619/how-to-use-socket-io-client-correctly-in-react-app-o65
     const socketClient = React.useContext(SocketContext);
 
     const [roomState, updateState] = React.useState({ games: [], players: [], gameJoined: false,  playingGameId: null, playerId: null });
     const [socket, updateSocket] = React.useState(null);
+    const [testState, setTestState] = React.useState(null);
+
+    console.log("test state "+ testState);
 
     const createGameHandler = (gameConfiguration) => {
         socket.emit('createGame', { width: gameConfiguration.width, height: gameConfiguration.height, maxPlayers: gameConfiguration.maxPlayers });
@@ -30,26 +36,40 @@ const GameRoom = (props) => {
     const socketInitialization = () => {
 
         socketClient.on("roomStateUpdate", data => {
+            console.log("running roomStateUpdate");
             updateState(prevState => ({...prevState, games: data.games, players: data.players }));
         });
 
         socketClient.on("playerIdAssigned", data => {
-
+            console.log("running playerIdAssigned");
             updateState(prevState => ({ ...prevState, playerId: data.playerId }));
         });
 
         socketClient.on("gameJoined", data => {
+            console.log("running gameJoined");
             updateState(prevState => ({ ...prevState, gamJoined: true, playingGameId: data.gameId }));
         });
 
-        //Once the socket is configured store it?
+        //Once effect is run component is rerendered due to the multiple setstates. setstate within useEffect
+        //doesnt stops effect and re-rerender. However, only
+        //last of each state prevails in the render.
+        console.log("setting test state");
+        setTestState(1)
+        console.log("setting socket state");
+        setTestState(2);    
+        console.log("setting socket state");
+        setTestState(3);    
+
         updateSocket(socketClient);
     };
 
     React.useEffect(() => {
 
+        console.log("Running use effect");
         socketInitialization();    
         socketClient.emit('newPlayerJoined', props.playerName);
+        console.log("Finishing use effect");
+
 
         return () => socketClient.disconnect();
     }, []);
